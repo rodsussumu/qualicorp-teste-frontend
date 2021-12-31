0b3d5a0d845f7bd2948ca2e718a4526956bf2afe
Rodrigo <rodsussumu@gmail.com>
2021-12-31 19:04:35 -0300

    Add default path

--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { BrowserRouter, Redirect, Route, Switch } from "react-router-dom";
 import RegisterCustomers from "./pages/Customers/RegisterCustomers";
 import ViewCustomers from "./pages/Customers/ViewCustomers";
 import UpdateCustomers from "./pages/Customers/UpdateCustomers";
@@ -8,6 +8,9 @@ export default function Routes() {
   return (
     <BrowserRouter>
       <Switch>
+        <Route exact path="/">
+          <Redirect to='/all/customers'></Redirect>
+        </Route>
         <Route path="/create/customers" component={RegisterCustomers} />
         <Route path="/all/customers" component={ViewCustomers} />
         <Route path="/update/customers/:id" component={UpdateCustomers} />
